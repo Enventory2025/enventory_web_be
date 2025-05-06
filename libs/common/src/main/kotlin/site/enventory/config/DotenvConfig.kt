96e8a8b4786793c6bd0bf7e2e5c5a5808711f8da
SongJSeop <wnstjq7979@gmail.com>
2025-05-06 23:24:56 +0900

    feat: AppController 추가

--- libs/common/src/main/kotlin/site/enventory/config/DotenvConfig.kt
+++ libs/common/src/main/kotlin/site/enventory/config/DotenvConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class DotEnvConfig {
+class DotenvConfig {
 
     @Bean
     fun dotenv(): Dotenv {
