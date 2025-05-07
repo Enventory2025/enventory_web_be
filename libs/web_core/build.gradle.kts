9ebe3e858debb3279262343b9c57ca56a4338fdd
SongJSeop <wnstjq7979@gmail.com>
2025-05-07 12:57:33 +0900

    refactor: docker-compose-local.yml postgres 설정에 volume 추가

--- libs/web_core/build.gradle.kts
+++ libs/web_core/build.gradle.kts
@@ -1,14 +1,10 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
-val jar: Jar by tasks
-val bootJar: BootJar by tasks
-
-bootJar.enabled = false
-jar.enabled = true
-
 dependencies {
     implementation(project(":common"))
 
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // kotlin logging
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
 //    // jwt
 //    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 //    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
