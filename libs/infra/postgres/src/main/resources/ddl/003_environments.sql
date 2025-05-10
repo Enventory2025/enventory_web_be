d4cbebe089dd568295357fada7298b4e6667ba1d
SongJSeop <wnstjq7979@gmail.com>
2025-05-10 17:07:29 +0900

    feat: EnvironmentEntity 추가

---
+++ libs/infra/postgres/src/main/resources/ddl/003_environments.sql
@@ -0,0 +1,9 @@
+CREATE TABLE environments (
+    id UUID PRIMARY KEY,
+    env_store_id UUID NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_environments_env_store_name ON environments(env_store_id, name);
