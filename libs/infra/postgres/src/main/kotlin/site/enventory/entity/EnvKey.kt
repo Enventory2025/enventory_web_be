6aeef67fa15696737cef320eed6309c29204302e
SongJSeop <wnstjq7979@gmail.com>
2025-05-10 17:10:59 +0900

    feat: EnvKeyEntity 추가

---
+++ libs/infra/postgres/src/main/kotlin/site/enventory/entity/EnvKey.kt
@@ -0,0 +1,22 @@
+package site.enventory.entity
+
+import com.github.f4b6a3.uuid.UuidCreator
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+import java.time.Instant
+import java.util.UUID
+
+@Table("env_keys")
+data class EnvKeyEntity(
+    @Id val id: UUID = UuidCreator.getTimeOrdered(),
+
+    @Column("env_store_id") val envStoreId: UUID,
+
+    val name: String,
+
+    @CreatedDate @Column("created_at") val createdAt: Instant = Instant.now(),
+    @LastModifiedDate @Column("updated_at") val updatedAt: Instant = Instant.now()
+)
---
+++ libs/infra/postgres/src/main/resources/ddl/004_env_keys.sql
@@ -0,0 +1,9 @@
+CREATE TABLE env_keys (
+    id UUID PRIMARY KEY,
+    env_store_id UUID NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_env_keys_env_store_name ON env_keys(env_store_id, name);
